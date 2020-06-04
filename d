f1b03e8daf1754a2b4de8ce97762ac81d2a5362b
rofigates <rofigates95@gmail.com>
2020-06-04 23:45:21 +0700

    bagian 2

---
+++ d
@@ -0,0 +1,11 @@
+[33mcommit d2054ec2753568350e7b8f3fd12d2e78f66decf0[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: rofigates <rofigates95@gmail.com>
+Date:   Mon May 18 01:07:13 2020 +0700
+
+    desain
+
+[33mcommit 62ccd58e7c0e71cf96d15cf7090b4f4f10bda4e5[m[33m ([m[1;31mFlutter/master[m[33m)[m
+Author: rofigates <rofigates95@gmail.com>
+Date:   Sun May 17 05:59:51 2020 +0700
+
+    first commit
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
 
+import './quiz.dart';
+import './result.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 20},
+        {'text': 'White', 'score': 30},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite Animal?',
+      'answer': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Cat', 'score': 10},
+        {'text': 'Lion', 'score': 10},
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answer': [
+        {'text': 'Rofi', 'score': 10},
+        {'text': 'Imron', 'score': 10},
+        {'text': 'Denny', 'score': 10},
+        {'text': 'Adhit', 'score': 10},
+      ]
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App'),),
-      body: Text("Default Text"),
-    ),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore,_resetQuiz),
+      ),
+    );
   }
 }
